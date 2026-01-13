8004b5f019a176ab531d7fdff16b97ba744a3278
Isaac <isaacz26@nycstudents.net>
2026-01-13 18:54:32 +0000

    Day5

---
+++ WDLesson3_3Practical/script.js
@@ -0,0 +1,15 @@
+/* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function showcss() {
+    document.getElementById("highlight").innerHTML =
+    "CSS"
+}
+
+function showhtml() {
+    document.getElementById("highlight").innerHTML =
+    "HTML"
+}
+
+function showjs() {
+    document.getElementById("highlight").innerHTML =
+    "JS"
+}
---
+++ WDLesson3_4Practical/script.js
@@ -0,0 +1,36 @@
+function createStory(){
+  //Following are variables for each of the <span>s that located throughout the story
+  let boy1span1 = document.getElementById("boyspan1");
+  let boy1span2 = document.getElementById("boyspan2");
+  let boy1span3 = document.getElementById("boyspan3");
+  let boy1span4 = document.getElementById("boyspan4");
+  let veg1 = document.getElementById("veg1");
+  let veg2 = document.getElementById("veg2");
+  let girlspan = document.getElementById("girlspan");
+
+  /* Task 3: Create variables and retrieve the information from the text inputs for the following, 
+      1) A boy's name
+      2) A vegetable
+      3) A girl's name
+      4) Another vegetable
+  */
+  let boy = document.getElementById("boy").value;
+  let girl = document.getElementById("girl").value;
+  let vegetable1 = document.getElementById("veggie1").value;
+  let vegetable2 = document.getElementById("veggie2").value;
+  /*
+  Choose your variable names wisely.  This is a lot of practice of creating variables and using document.getElementById(..)
+  */
+
+  /* Task 4: Set the innerHTML for each of the spans on lines 3 to 9 with the appropriate values from the variables you create in Task 3. */
+boy1span1.innerHTML = boy;
+boy1span2.innerHTML = boy;
+boy1span3.innerHTML = boy;
+boy1span4.innerHTML = boy;
+veg1.innerHTML = vegetable1;
+veg2.innerHTML = vegetable2;
+girlspan.innerHTML = girl;
+  /* Task 5: Set the display property of the element with an id of "story" to 'block' */
+
+
+}
