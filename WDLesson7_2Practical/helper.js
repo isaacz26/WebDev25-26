d21c1bbb86b8096d390933489295ab435376f07f
Isaac <isaacz26@nycstudents.net>
2026-05-19 17:56:40 +0000

    e

--- WDLesson7_2Practical/helper.js
+++ WDLesson7_2Practical/helper.js
@@ -21,9 +21,9 @@ function showMap(lat,lon){
 // Challenge 4: Create the function card() to generate an appropriate info card with the button to view map
 function card( info ){ 
    let build = `<div class="card fitted">
-                  <h3>${info.crash_date}</h3>
+                  <h3>${info.on_street_name}</h3>
                   <hr>
-                  <p>Location: ${info.on_street_name}</p>
+                  <p>Location: ${info.off_street_name}</p>
                   <h5>${info.borough}</h5>`;
                   if(info.latitude && info.longitude){
                     build += `<input type="button" value="Map" onclick="showMap( ${info.latitude}, ${info.longitude} )">`;
